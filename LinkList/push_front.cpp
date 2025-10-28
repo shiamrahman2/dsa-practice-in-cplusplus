9da75078cee5643fc0c260b42a255143abde7e88
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-10-28 23:16:00 +0600

    print Link list

--- LinkList/push_front.cpp
+++ LinkList/push_front.cpp
@@ -36,6 +36,14 @@ class List{
         tail=newNode;// update tail value
      }
   }
+  void printList(){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;// update temp
+    }
+    cout<<"NULL"<<endl;;
+  }
 };
 int main()
 {   
@@ -43,8 +51,10 @@ int main()
     ll.push_front(3);
     ll.push_front(2);
     ll.push_front(1);
+    ll.printList();//output:1->2->3->NULL
     ll.push_back(4);
     ll.push_back(5);
+    ll.printList();
     // output 1->2->3->4->5->null;
     return 0;
 }
