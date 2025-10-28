7f568b0f2b8423b00cfd24a7168626013d52c11c
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-10-29 01:50:33 +0600

    size and delete Nth Node from End of Link List implement

---
+++ LinkList/DeleteNthNode.cpp
@@ -0,0 +1,137 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+  public:
+      int data;
+      Node* next;
+     Node(int value){
+        data=value;
+        next=NULL;
+     }
+};
+class List{
+    Node* head;
+    Node* tail;
+  public:
+   List(){
+         head=NULL;// initially head point null value
+         tail=NULL;
+  }
+  void push_front(int value){
+      Node* newNode=new Node(value);
+       if(head==NULL){
+           head=tail=newNode;
+       }else{
+           newNode->next=head;
+           head=newNode;
+       }
+  }
+  void push_back(int value){
+     Node* newNode=new Node(value);// initially Node create
+     if(head==NULL){// if List Empty
+         head=tail=newNode;
+     }else{
+        tail->next=newNode;
+        tail=newNode;// update tail value
+     }
+  }
+  void printList(){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;// update temp
+    }
+    cout<<"NULL"<<endl;;
+  }
+  void insert(int value,int position){
+     Node* newNode=new Node(value);
+     Node* temp=head;
+     for(int i=0;i<position-1;i++){
+         temp=temp->next;
+     }
+     if(temp==NULL){
+        cout<<"Postion Invalid";
+        return;
+     }else{
+        newNode->next=temp->next;
+        temp->next=newNode;
+     }
+  }
+  void pop_front(){
+     Node*temp=head;
+     head=head->next;
+     temp->next=NULL;
+     delete temp;
+  }
+  void pop_back(){
+    Node*temp=head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=NULL;// temp indicate 2nd last Node;
+    delete tail;
+    tail=temp;
+  }
+  int helper(Node* temp,int key){
+    if(temp==NULL){
+        return -1;
+    }
+      if(temp->data==key){
+        return 0;
+      }
+      int idx=helper(temp->next,key);
+      if(idx==-1){
+        return -1;
+      }
+      return idx+1;
+  }
+  int searchRec(int key){
+    return helper(head,key);
+  }
+  void reverse(){
+    Node*curr=head;
+    Node*prev=NULL;
+    while(curr!=NULL){
+        Node* next=curr->next;
+        curr->next=prev;//reverse Link;
+        prev=curr;
+        curr=next;
+    }
+    // prev indicate last Node
+    head=prev;
+  }
+  int size(){
+    int sz=0;
+    Node*temp=head;
+    while(temp!=NULL){
+        sz++;
+        temp=temp->next;
+    }
+    return sz;
+  }
+  void deleteNthNode(int n){
+    int sz=size();
+    Node* prev=head;
+    for(int i=0;i<(sz-n);i++){
+        prev=prev->next;
+    }
+    Node *deleteNode=prev->next;
+    cout<<"Node going to delete:"<<deleteNode->data<<endl;
+    prev->next=prev->next->next;
+  }
+};
+int main()
+{   
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.push_back(4);
+    ll.push_back(5);
+     ll.insert(100,2);
+     ll.printList();
+    ll.deleteNthNode(3);
+    ll.printList();
+    return 0;
+}
