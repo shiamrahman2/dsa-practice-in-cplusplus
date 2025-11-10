b4b6a8b9531db07befbc6105f411039354cd22e9
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-11-10 23:59:37 +0600

    Reverse Queue Using Stack

---
+++ Queue/ReverseQueue.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverseQueue(queue<int> &q){
+     stack<int>s;
+     while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+     }
+     while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+     }
+
+}
+int main()
+{   
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    reverseQueue(q);
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    return 0;
+}
