14ff0aa2d4703982d1d415a3d38b111c8475802a
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-11-14 11:56:36 +0600

    Maximum Length of Chain

---
+++ Greedy/MaximumLegnthOfPairs.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool compare(pair<int,int>p1,pair<int,int>p2){
+    return p1.second<p2.second;
+}
+int maxLengthChain(vector<pair<int,int>>pairs){
+         
+    int n=pairs.size();
+    sort(pairs.begin(),pairs.end(),compare);
+    int ans=1;
+    int currentSecond=pairs[0].second;
+    for(int i=1;i<n;i++){
+        if(pairs[i].first>currentSecond){
+            ans++;
+            currentSecond=pairs[i].second;
+        }
+    }
+    return ans;
+}
+int main()
+{   
+    int n=5;
+    vector<pair<int,int>>pairs(n,make_pair(0,0));
+    pairs[0]=make_pair(5,24);
+    pairs[1]=make_pair(39,60);
+    pairs[2]=make_pair(5,28);
+    pairs[3]=make_pair(27,40);
+    pairs[4]=make_pair(50,90);
+    int maxLength=maxLengthChain(pairs);
+    cout<<"Max Chain Length:"<<maxLength<<endl;
+    return 0;
+}
