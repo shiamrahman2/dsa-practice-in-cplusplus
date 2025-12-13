690dbf637c0ec3bf10e74cf8e15a42710f8142d5
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-12-14 01:44:05 +0600

    Unoredered Set

---
+++ Hashing/Unordered_set.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    unordered_set<int>s;
+    s.insert(1);
+    s.insert(5);
+    s.insert(1);
+    s.insert(5);
+    s.insert(4);
+    s.insert(7);
+    cout<<s.size()<<endl;
+    for(auto val:s){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    if(s.find(5)!=s.end()){
+        cout<<"value exist"<<endl;
+    }else{
+        cout<<"value Doesn't Exist"<<endl;
+    }
+
+    return 0;
+}
