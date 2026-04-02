f2c15997316ef5ca9bf16ebb930892bab56efa8b
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2026-04-02 19:25:57 +0600

    Bubble Sort Revision

--- Basic_Sorting_Algo/bubbleSort.cpp
+++ Basic_Sorting_Algo/bubbleSort.cpp
@@ -1,17 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-void bubbleSort(vector<int> &v){
+// void bubbleSort(vector<int> &v){
+//     int n=v.size();
+//     for(int i=0;i<n-1;i++){
+//         bool isSwap=false;
+//         for(int j=0;j<n-i-1;j++){
+//             if(v[j]>v[j+1]){
+//                 swap(v[j],v[j+1]);
+//                 isSwap=true;
+//             }
+//         }
+//         if(!isSwap){
+//             return;// bcz array is already swap;
+//         }
+//     }
+// }
+void bubbleSort(vector<int>&v){
     int n=v.size();
     for(int i=0;i<n-1;i++){
-        bool isSwap=false;
         for(int j=0;j<n-i-1;j++){
+            bool isSwap=false;
             if(v[j]>v[j+1]){
                 swap(v[j],v[j+1]);
                 isSwap=true;
             }
-        }
-        if(!isSwap){
-            return;// bcz array is already swap;
+            if(!isSwap){
+                return;
+            }
         }
     }
 }
