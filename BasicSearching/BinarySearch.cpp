ce9415cb681f9d3b2895ec269611a8263211b6eb
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2026-06-14 20:40:45 +0600

    BinarySearch

---
+++ BasicSearching/BinarySearch.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 6, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int target = 6;
+
+    int index = binarySearch(arr, n, target);
+
+    if (index != -1)
+        cout << "Found at index: " << index << endl;
+    else
+        cout << "Not Found" << endl;
+
+    return 0;
+}
